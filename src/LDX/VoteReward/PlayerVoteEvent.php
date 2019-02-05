f567fdccda6ee5815e1ef6f782bf7366b056ca6f
MCPE_PC <maxpjh0528@naver.com>
2019-02-06 00:10:46 +0900

    Tired ;/

--- src/LDX/VoteReward/PlayerVoteEvent.php
+++ src/LDX/VoteReward/PlayerVoteEvent.php
@@ -2,17 +2,19 @@
 
 namespace LDX\VoteReward;
 
-use LDX\VoteReward\Main as VoteReward;
 use pocketmine\event\plugin\PluginEvent;
 use pocketmine\event\Cancellable;
 use pocketmine\Player;
 
 class PlayerVoteEvent extends PluginEvent implements Cancellable {
-    public static $handlerList = null;
-    protected $player;
+  public static $handlerList = null;
+  protected $player;
 
-    public function __construct(VoteReward $plugin, Player $player, $data) {
-        parent::__construct($plugin);
-        $this->player = $player;
-    }
+  public function __construct(VoteReward $plugin, Player $player, $data) {
+    parent::__construct($plugin);
+    $this->player = $player;
+  }
+  public function getPlayer(): Player {
+    return $this->player;
+  }
 }
